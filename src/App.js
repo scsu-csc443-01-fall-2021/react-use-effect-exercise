c38d932fc84be194128aea2a595a768b40902cfc
Michael Clark <clarkm1@southernct.edu>
2021-10-11 21:20:27 -0400

    Starter files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+const saveUsersToLocal = (users) =>
+  localStorage.setItem("users", JSON.stringify(users));
+
+const loadUsersFromLocal = () =>
+  JSON.parse(localStorage.getItem("users") || "[]");
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
